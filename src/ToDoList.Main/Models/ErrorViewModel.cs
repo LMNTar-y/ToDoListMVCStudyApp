620e7ccbf43f901acfcbfd1ea7e5a736813021ab
Aliaksei Balvanovich <a.balvanovich@gmail.com>
2022-09-08 19:28:56 +0300

    Initial commit

---
+++ src/ToDoList.Main/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ToDoList.Main.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
